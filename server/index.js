cafa3e26760d6c52bc8eb90a44e6d46a5e9e3af6
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-05-21 16:57:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import weatherRoutes from './routes/api/records.js';
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use('/api/records', weatherRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.OPENWEATHER_API_KEY;
